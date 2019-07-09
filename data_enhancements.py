807eca1b525d4624e6b65701df4e7c25dc04ba8f
Anuj Jain <lookforaj@gmail.com>
2019-07-09 14:10:32 +0530

    Removed sounds effects/expressions/gestures
    Using iterrows() to traverse entire dataset

--- data_enhancements.py
+++ data_enhancements.py
@@ -17,14 +17,21 @@ dataset = dataset[dataset['deleted'] == False]
 # Merge David and David Wallace
 dataset['speaker'] = dataset['speaker'].replace('David', 'David Wallace')
 
+ids = []
 
 # Remove sounds effects/expressions/gestures like [laughing], [birds chirping], [nods]
+print('Trying to remove sounds effects/expressions/gestures..')
+print('I am using iterrows() to traverse entire dataset which is apparently very slow, pls suggest an faster alternative..')
 temp = pd.DataFrame([], columns=[])
 for index, row in dataset.iterrows():
-    if row['line_text'][0] == '[' and row['line_text'][len(row['line_text'])-1] == ']':
-        string = row['line_text']
+    string = row['line_text']
+    if row['id']%1000 == 0:
+        print(row['id'], ' rows processed out of 60,000...')
+    if re.search(r"[\[].*?[\]]", string) != "None":      
         stripped = re.sub(r"[\[].*?[\]]", "", string, flags=re.X)
         stripped = stripped.strip()
+        if len(stripped) == 0:
+            dataset = dataset.drop(index)
         if len(stripped) > 0:
             row['line_text'] = stripped
             temp = temp.append(row, ignore_index=True)
